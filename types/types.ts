fb033f0f5a448e1b181884ed0db9a7685b638dd8
WonhyeongLee <bigfile57@naver.com>
2022-11-22 19:17:12 +0900

    :adhesive_bandage: User의 타입수정
    workspaces도 추가해야하는데 일단 임시 수정(타입스크립트 이해부족)

--- types/types.ts
+++ types/types.ts
@@ -1,10 +1,11 @@
-export interface Id{
-  id: number;
-}
+// export interface Id{
+//   id: number;
+// }
 export interface NewUser {
+  id: number;
   email: string;
   nickname?: string;
   password?: string;
 }
 
-export type User = Id & NewUser;
+export type User = NewUser;
