9eedaccd88027c384ff7b5b6030e1b3ff23948d6
WonhyeongLee <bigfile57@naver.com>
2022-11-22 19:47:58 +0900

    :sparkles: Channel 페이지 컴포넌트 작성

---
+++ pages/Channel/index.tsx
@@ -0,0 +1,12 @@
+import Workspace from '@layouts/Workspaces';
+import React from 'react';
+
+const Channel = () => {
+  return (
+    <Workspace>
+      <div>환영합니다!</div>
+    </Workspace>
+  );
+};
+
+export default Channel;
